21be7a54c11891ce2b43c0d0564dae332fca8642
mohammadmajed <62719469+mohammadmajed@users.noreply.github.com>
2022-11-06 13:12:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ $("#todoform").submit(function (e) {
     e.preventDefault();
 });
 
-
 $('#btn-add').click(() => {
     let Formtd = $('#form1').val();
     $('#tb-item tbody').append(`
-    <tr data-id="${count}">
-        <td>${Formtd}</td>
-        <td>in progrees</td>
-        <td><button>delete</button></td>
+        <tr data-id="${count}">
+            <td><input type='checkbox'></td>
+            <td>${Formtd}</td>
+            <td>in progrees</td>
+            <td><button class='btn btn-danger'>delete</button></td>
         </tr>`);
         $(`tr[data-id="${count}"] button`).click(function () {
             $(this).parent().parent().remove();
+        ;})
+        $(`tr[data-id="${count}"] input`).click(function () {
+            if ($(this).parent().parent().css('text-decoration-line') == "none")  
+            $(this).parent().parent().css('text-decoration-line', 'line-through');  
+        else  
+            $(this).parent().parent().css('text-decoration-line', 'none');  
         ;})
         
     count++;
 
 
 });
-
-
-
-
-
-    
-
 
